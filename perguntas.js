3ee9b395197833ae9a4e4d9ce68c736a0dc329de
csmicxa <143115310+csmicxa@users.noreply.github.com>
2024-10-11 12:00:00 -0300

    s

--- perguntas.js
+++ perguntas.js
@@ -46,34 +46,34 @@ criaCartao ('Inglês',
  'How do you form the negative of “She likes pizza” in the present simple?', 
  'She does not like pizza.')
 
-criaCartão( 'biologia',
+criaCartao( 'biologia',
  'O que é seleção natural?',
  'A seleção natural é o processo pelo qual organismos com características mais adaptadas ao ambiente têm maior chance de sobreviver e se reproduzir, transmitindo essas características para as próximas gerações')
 
-criaCartão('biologia',
+criaCartao('biologia',
  'O que é mitose e qual sua importância para os organismos',
  'A mitose é um processo de divisão celular que resulta em duas células-filhas geneticamente idênticas à célula-mãe. Ela é importante para o crescimento dos organismos, reparação de tecidos e substituição de células mortas. É essencial para a manutenção do número de cromossomos em células somáticas.')
 
-criaCartão('biologia',
+criaCartao('biologia',
  'Qual é a função dos ribossomos na célula?',
  'Os ribossomos são responsáveis pela síntese de proteínas, traduzindo as informações do RNA mensageiro para produzir cadeias de aminoácidos que formam as proteínas')
 
-criaCartão('biologia',
+criaCartao('biologia',
  'Qual a função do sistema imunológico?',
  'O sistema imunológico protege o organismo contra patógenos, como vírus e bactérias, identificando e eliminando esses agentes invasores para manter a saúde do corpo')
 
-criaCartão('geografia',
+criaCartao('geografia',
  'O que é globalização?',
  'Globalização é o processo de integração econômica, social, cultural e política entre os países, facilitado pelo avanço da tecnologia e comunicação')
 
-criaCartão('geografia',
+criaCartao('geografia',
  'O que são fusos horários?',
  'Fusos horários são divisões da Terra em 24 partes, cada uma com uma hora diferente, baseadas na rotação do planeta')
 
-criaCartão('geografia',
+criaCartao('geografia',
  'O que é clima?',
  'Clima é a média das condições atmosféricas de uma região ao longo do tempo, como temperatura e precipitação')
 
-criaCartão('geografia',
+criaCartao('geografia',
  'O que é agricultura de subsistência?',
  'Agricultura de subsistência é o tipo de agricultura em que a produção é voltada principalmente para o consumo da família ou da comunidade, com pouca ou nenhuma venda de excedentes')
