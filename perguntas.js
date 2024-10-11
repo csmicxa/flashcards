fc60c49331f425ee654b4838ffc352941fcd6288
csmicxa <143115310+csmicxa@users.noreply.github.com>
2024-10-11 11:44:57 -0300

    f

---
+++ perguntas.js
@@ -0,0 +1,79 @@
+criaCartao ('História',
+ 'Qual foi o documento forjado pelo capitão Olímpio Mourão Filho usado como justificativa por Getúlio Vargas para implantar uma ditadura no Brasil em 1937?',
+ 'Plano Cohen')
+
+criaCartao ('História',
+ 'Quem Assumiu o governo em 1946 após a saída de Vargas?',
+ 'Dutra')
+
+criaCartao ('História',
+ 'Partido que desejava depor Getúlio em 1954',
+ 'O Udn')
+
+criaCartao ('História',
+ 'Na Segunda Guerra Mundial, o Brasil combateu ao lado dos',
+ 'Aliados')
+
+criaCartao ('Química',
+ 'Quando uma cadeia carbônica é aromática?',
+ 'Quando sua estrutura é cíclica')
+
+criaCartao ('Química',
+ 'Quais são as funções nitrogenadas?',
+ 'Amina e Amida')
+
+criaCartao ('Química',
+ 'Quantas ligações o Carbono faz?',
+ 'Quatro ligações')
+
+criaCartao ('Química',
+ 'Qual a diferença entre reação endotérmicas e exotérmicas?',
+ 'As reações endotérmicas absorvem calor, já as exotérmicas liberam')
+
+criaCartao ('Inglês',
+ 'What is the past tense of “go” ?', 
+ 'Went')
+
+criaCartao ('Inglês',
+ 'What is the plural form of “child”?', 
+ 'Children')
+
+criaCartao ('Inglês', 
+ 'Complete with the correct preposition: “She is good ___ math.”',
+ 'At')
+
+criaCartao ('Inglês',
+ 'How do you form the negative of “She likes pizza” in the present simple?', 
+ 'She does not like pizza.')
+
+criaCartão( 'biologia',
+ 'O que é seleção natural?',
+ 'A seleção natural é o processo pelo qual organismos com características mais adaptadas ao ambiente têm maior chance de sobreviver e se reproduzir, transmitindo essas características para as próximas gerações')
+
+criaCartão('biologia',
+ 'O que é mitose e qual sua importância para os organismos',
+ 'A mitose é um processo de divisão celular que resulta em duas células-filhas geneticamente idênticas à célula-mãe. Ela é importante para o crescimento dos organismos, reparação de tecidos e substituição de células mortas. É essencial para a manutenção do número de cromossomos em células somáticas.')
+
+criaCartão('biologia',
+ 'Qual é a função dos ribossomos na célula?',
+ 'Os ribossomos são responsáveis pela síntese de proteínas, traduzindo as informações do RNA mensageiro para produzir cadeias de aminoácidos que formam as proteínas')
+
+criaCartão('biologia',
+ 'Qual a função do sistema imunológico?',
+ 'O sistema imunológico protege o organismo contra patógenos, como vírus e bactérias, identificando e eliminando esses agentes invasores para manter a saúde do corpo')
+
+criaCartão('geografia',
+ 'O que é globalização?',
+ 'Globalização é o processo de integração econômica, social, cultural e política entre os países, facilitado pelo avanço da tecnologia e comunicação')
+
+criaCartão('geografia',
+ 'O que são fusos horários?',
+ 'Fusos horários são divisões da Terra em 24 partes, cada uma com uma hora diferente, baseadas na rotação do planeta')
+
+criaCartão('geografia',
+ 'O que é clima?',
+ 'Clima é a média das condições atmosféricas de uma região ao longo do tempo, como temperatura e precipitação')
+
+criaCartão('geografia',
+ 'O que é agricultura de subsistência?',
+ 'Agricultura de subsistência é o tipo de agricultura em que a produção é voltada principalmente para o consumo da família ou da comunidade, com pouca ou nenhuma venda de excedentes')
